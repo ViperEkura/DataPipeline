fec37545e2d211f7eb2293bb40677cade2de6957
ViperEkura <3081035982@qq.com>
2026-07-06 08:53:18 +0800

    Add max_chunks=18 to dolma-30b export, remove max_chunks=5 from english-wiki, rename project to datapipline

--- scripts/pre_train/english-dolma.py
+++ scripts/pre_train/english-dolma.py
@@ -2,9 +2,10 @@ from datasets import load_dataset
 from pipeline import export_dataset
 
 if __name__ == "__main__":
-    dataset = load_dataset("Blaze7451/enwiki_structured_content")
+    dataset = load_dataset("emozilla/dolma-v1_7-30B")
     export_dataset(
         dataset=dataset["train"],
         output_dir="./dataset",
-        output_prefix="english-wiki-pretrain",
+        output_prefix="english-dolma-30b-pretrain",
+        max_chunks=18,
     )
